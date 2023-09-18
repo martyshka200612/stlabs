65245c8bd069a8b9fd0b4a68d7abb001bd29a729
starikkovich <144004899+starikkovich@users.noreply.github.com>
2023-09-18 14:07:31 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    double x;
+    int k;
+    int i=0;
+    std::cin >> x;
+    std::cin >> k;
+    double s = sin (x) / x, st = 1;
+    std::cout << setprecision(k + 1) << s << std::endl;
+    double a = 1;
+
+
+        while (int(s* pow(10,k)) != int(st* pow(10,k))) {
+            a *= (-1) * pow(x,2)/(2*i + 3)/(2*i+2);
+            i++;
+            st = st +  a;
+            std::cout << setprecision(k + 1) << st << endl;
+        }
+
+    }
+
+
