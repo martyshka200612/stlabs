2789c6795577d2472a173e082e73918f452900ee
starikkovich <144004899+starikkovich@users.noreply.github.com>
2023-10-29 21:56:31 +0300

    Update laba5.cpp

--- laba5.cpp
+++ laba5.cpp
@@ -3,52 +3,47 @@
 #include <fstream>
 #include <string>
 using namespace std;
- 
-int main()
-{
-    ifstream fin("input1.txt");
+
+int main() {
+    ifstream fin("comm.txt");
     ofstream fout("output1.txt");
-    if (!fin.is_open())
-    {
+    if (!fin.is_open()) {
         fout << "Error opening file" << endl;
+        return 1;
     }
-    else
-    {
-        string str;
-        bool notagroup = true;
-        bool group = false;
-        while (getline(fin, str))
-        {
-            for (int i = 0; i < str.length(); i++)
-            {
-                if (str[i] != ' ')
-                {
-                    if ((str[i] == '/') && (str[i + 1] == '/'))
-                    {
-                        str.erase(0, i);
-                        group = true;
-                        if ((notagroup) && (group))
-                        {
-                            fout << "Comments" << endl;
-                            group = false;
-                            notagroup = false;
-                        }
-                        fout << str << endl;
-                        break;
-                    }
-                    else
-                    {
-                        notagroup = true;
-                        group = false;
-                        break;
-                    }
- 
+    int i;
+    string str;
+    bool LineIsComment;
+    while (getline(fin, str)) {
+        for (i = 0; i < str.length(); i++) {
+            if (str[i] != ' ') {
+                if ((str[i] == '/') && (str[i + 1] == '/')) {
+                    LineIsComment = true;
                 }
             }
         }
-        fin.close();
-        fout.close();
     }
- 
-    return 0;
+
+    int groupStartedLine = 0;
+    int groupCommSize = 0;
+    string repeatedLine = "";
+    for (int k = 0; k < str.size(); k++) {
+        if (LineIsComment == true) {
+            if (groupCommSize == 0) {
+                groupStartedLine = k;
+                repeatedLine = str[k];
+            }
+            groupCommSize++;
+        } else {
+            if (groupCommSize > 0) {
+                fout << "The group starts with the line " << groupStartedLine + 1;
+                fout << ", number of repeated line: " << groupCommSize << endl;
+                groupCommSize = 0;
+            }
+        }
+    }
+    if (groupCommSize > 0) {
+        fout << "The group starts with the line " << groupStartedLine + 1;
+        fout << ", number of repeated line: " << groupCommSize << endl;
+    }
 }
