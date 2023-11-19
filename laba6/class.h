839d33dd6716c358e1f14987118d3eb721e7bc92
starikkovich <144004899+starikkovich@users.noreply.github.com>
2023-11-19 15:50:28 +0300

    Create class.h

---
+++ laba6/class.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class ProperFraction {
+private:
+    int num;
+    int denom;
+
+    int gcd(int i, int j);
+
+    void simplify();
+
+public:
+    ProperFraction(int numerator, int denominator);
+    ProperFraction(const ProperFraction& fraction);
+
+
+    ProperFraction operator+ (const ProperFraction& fraction);
+
+    ProperFraction operator* (const ProperFraction& fraction);
+
+    ProperFraction operator/ (const ProperFraction& fraction);
+
+
+    int getNumerator(){
+        return num;
+    }
+
+    int getDenominator(){
+        return denom;
+    }
+
+    void print();
+};
